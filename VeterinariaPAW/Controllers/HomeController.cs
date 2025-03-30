b88fb1cbdd805e7ccbe9f9b45b6bbf3b24adb646
JoseBravo2003 <josebravogamboa42@gmail.com>
2025-03-29 23:08:19 -0600

    Cambios en pagina principal, consultas y productos

--- VeterinariaPAW/Controllers/HomeController.cs
+++ VeterinariaPAW/Controllers/HomeController.cs
@@ -17,11 +17,12 @@ namespace VeterinariaPAW.Controllers
         {
             return View();
         }
-
-        public IActionResult Privacy()
+        public IActionResult Consulta()
         {
             return View();
         }
+
+        
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
